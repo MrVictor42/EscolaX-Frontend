b7509ecbc6fe0af9d72eccbfc6ad182b3ce11e11
MrVictor42 <guin-hu@hotmail.com>
2022-06-23 18:46:01 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,75 +1,62 @@
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/templates/header/header.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FooterComponent } from './components/templates/footer/footer.component';
-import { NavComponent } from './components/templates/nav/nav.component';
+import { AppComponent } from './app.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { CoreModule } from '@core/core.module';
+import { ThemeModule } from '@theme/theme.module';
+import { SharedModule } from '@shared/shared.module';
+import { RoutesModule } from './routes/routes.module';
+import { FormlyConfigModule } from './formly-config.module';
+import { NgxPermissionsModule } from 'ngx-permissions';
+import { ToastrModule } from 'ngx-toastr';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import { HomeComponent } from './views/home/home.component';
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
+import { environment } from '@env/environment';
+import { BASE_URL, httpInterceptorProviders, appInitializerProviders } from '@core';
 
-import localePt from '@angular/common/locales/pt';
-import { registerLocaleData } from '@angular/common';
-import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
-import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component'; 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemDataService } from '@shared/in-mem/in-mem-data.service';
 
-registerLocaleData(localePt)
+// Required for AOT compilation
+export function TranslateHttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		HeaderComponent,
-		FooterComponent,
-		NavComponent,
-		HomeComponent,
-		ProductCrudComponent,
-		ProductCreateComponent,
-  ProductReadComponent,
-  ProductUpdateComponent,
-  ProductDeleteComponent
-	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		MatToolbarModule,
-		MatSidenavModule,
-		MatListModule,
-		MatCardModule,
-		MatButtonModule,
-		MatSnackBarModule,
-		HttpClientModule,
-		FormsModule,
-		MatFormFieldModule,
-		MatInputModule,
-		MatTableModule,
-		MatPaginatorModule,
-		MatSortModule
-	],
-	providers: [{
-		provide: LOCALE_ID,
-		useValue: 'pt-BR'
-	}],
-	bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
+    ThemeModule,
+    RoutesModule,
+    SharedModule,
+    FormlyConfigModule.forRoot(),
+    NgxPermissionsModule.forRoot(),
+    ToastrModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: TranslateHttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    // Demo purposes only for GitHub Pages
+    HttpClientInMemoryWebApiModule.forRoot(InMemDataService, {
+      dataEncapsulation: false,
+      passThruUnknownUrl: true,
+    }),
+  ],
+  providers: [
+    { provide: BASE_URL, useValue: environment.baseUrl },
+    httpInterceptorProviders,
+    appInitializerProviders,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/product/product.model.ts
+++
@@ -1,5 +0,0 @@
-export interface Product {
-    id?: number, //optional
-    name: string,
-    price?: number
-}
