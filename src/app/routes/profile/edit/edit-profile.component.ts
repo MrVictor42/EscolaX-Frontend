9c3ddbf1d59a14348c06de13815ca231442a7384
MrVictor42 <guin-hu@hotmail.com>
2022-07-12 00:36:14 -0300

    Avatar

--- src/app/routes/profile/edit/edit-profile.component.ts
+++ src/app/routes/profile/edit/edit-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { AuthService, User } from '../../../core/authentication';
 
@@ -10,25 +11,44 @@ import { AuthService, User } from '../../../core/authentication';
 export class ProfileEditComponent implements OnInit {
 
 	@Input() user: User = new User();
+	urlAvatar: string = "../../../../assets/images/avatar.jpg";
+	isNull: boolean = false;
+	eventSelected: any;
 
-	constructor(private authService: AuthService) {
+	constructor(private authService: AuthService, private snackbar: MatSnackBar) {
 
 	}
 
 	ngOnInit(): void {
-
+		this.user.avatar ? null : this.isNull = true;
 	}
 
-	onSubmit(event: Event, user: User) {
-		const target = event.target as HTMLInputElement;
-		const files = target.files as FileList;
-		if (files) {
-			const foto = files[0];
+	onSubmit(user: User) {
+		if (this.eventSelected != null) {
 			const formData: FormData = new FormData();
-			formData.append("foto", foto);
+			const target = this.eventSelected.target.files;
+			const avatar = target.item(0);
 
-			console.log(user)
-			console.log(formData)
+			formData.append('avatar', avatar)
+
+			formData.forEach((value, key) => {
+				console.log(key + " " + value)
+			});
+		}
+	}
+
+	changePhoto(event: Event) {
+		const target: any = event.target as HTMLInputElement;
+		const file: File = (target.files as FileList)[0];
+		this.eventSelected = event;
+
+		if (file) {
+			const reader: FileReader = new FileReader();
+
+			reader.readAsDataURL(file);
+			reader.onload = (event: any) => {
+				this.urlAvatar = event.target.result;
+			}
 		}
 	}
 }
